633c4657a71a96611735eecf06720e3f4fb05378
TejaswiniHegdeP <tejaswini.hegdep@gmail.com>
2025-03-30 12:36:34 +0530

    Add Source Files

---
+++ script.js
@@ -0,0 +1,25 @@
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function currentSlide(n) {
+    showSlides(slideIndex = n - 1);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("slides");
+    let dots = document.getElementsByClassName("dot");
+
+    if (n >= slides.length) { slideIndex = 0; }
+    if (n < 0) { slideIndex = slides.length - 1; }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].classList.remove("active");
+    }
+
+    slides[slideIndex].style.display = "block";
+    dots[slideIndex].classList.add("active");
+}
